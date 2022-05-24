2a6bf5bb9b5402c0b21a112124c782541af9267f
bc090402389@gmail.com <bc090402389@gmail.com>
2022-05-24 22:23:09 +0500

    Changes

---
+++ src/app/practice-form/practice-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-practice-form',
+  templateUrl: './practice-form.component.html',
+  styleUrls: ['./practice-form.component.css']
+})
+export class PracticeFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
