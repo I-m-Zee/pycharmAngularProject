ba548b3361d00becd084f2e477178077f4305f4a
bc090402389@gmail.com <bc090402389@gmail.com>
2022-05-24 22:23:22 +0500

    Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PracticeFormComponent } from './practice-form/practice-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PracticeFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/practice-form/practice-form.component.ts
+++ src/app/practice-form/practice-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-practice-form',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PracticeFormComponent implements OnInit {
 
-  constructor() { }
+  public form:FormGroup;
+  constructor(fb:FormBuilder) {
+    this.form=fb.group({
+      userName:[null,Validators.required]
+    })
+   }
 
   ngOnInit(): void {
   }
